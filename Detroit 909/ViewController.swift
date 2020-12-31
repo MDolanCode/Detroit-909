62cdf6f4442de7f4442bfbdf9694e13ff302ba9d
Matt Dolan <mdolan.code@gmail.com>
2020-12-31 15:25:20 -0800

    Add IBOutlets for Detroit 909 Label and drumMachineButtons. Add IBAction and hook up all drumMachineButtons. Add view background colour and button colour for drumMachineButtons programatically.

--- Detroit 909/ViewController.swift
+++ Detroit 909/ViewController.swift
@@ -10,11 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var detroit909Label: UILabel!
+    @IBOutlet weak var bassDrumButton: UIButton!
+    @IBOutlet weak var snareDrumButton: UIButton!
+    @IBOutlet weak var lowTomButton: UIButton!
+    @IBOutlet weak var midTomButton: UIButton!
+    @IBOutlet weak var hiTomButton: UIButton!
+    @IBOutlet weak var rimButton: UIButton!
+    @IBOutlet weak var clapButton: UIButton!
+    @IBOutlet weak var closedHatButton: UIButton!
+    @IBOutlet weak var openHatButton: UIButton!
+    @IBOutlet weak var crashButton: UIButton!
+    @IBOutlet weak var rideButton: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        view.backgroundColor = .black
     }
 
-
+    @IBAction func drumMachineButtonPressed(_ sender: UIButton) {
+        //sender.backgroundColor = UIColor.red
+    }
+    
+    func drumMachineButtonUI() {
+        bassDrumButton.backgroundColor = .white
+        snareDrumButton.backgroundColor = .white
+        lowTomButton.backgroundColor = .white
+        midTomButton.backgroundColor = .white
+        hiTomButton.backgroundColor = .white
+        rimButton.backgroundColor = .white
+        clapButton.backgroundColor = .white
+        closedHatButton.backgroundColor = .white
+        openHatButton.backgroundColor = .white
+        crashButton.backgroundColor = .white
+        rideButton.backgroundColor = .white
+    }
+    
 }
 
