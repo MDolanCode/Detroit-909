7133ac56c0e2c0c2df64106e26d17a4b38b321f0
Matt Dolan <mdolan.code@gmail.com>
2021-02-12 17:04:09 -0800

    Create SettingsViewController and segue.

---
+++ Detroit 909/Controller/SettingsViewController.swift
@@ -0,0 +1,28 @@
+//
+//  SettingsViewController.swift
+//  Detroit 909
+//
+//  Created by Matt Dolan External macOS on 2021-02-12.
+//  Copyright © 2021 Matt Dolan External macOS. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
