8da0599cb4e8358db0d47d45692076617bb05294
Matt Dolan <mdolan.code@gmail.com>
2021-02-15 11:34:16 -0800

    Setup navigation bar and table view.

--- Detroit 909/Controller/SettingsViewController.swift
+++ Detroit 909/Controller/SettingsViewController.swift
@@ -10,19 +10,39 @@ import UIKit
 
 class SettingsViewController: UIViewController {
 
+    @IBOutlet weak var backButton: UIBarButtonItem!
+    @IBOutlet weak var settingsTableView: UITableView!
+    
+    var settingsList = ["FAQ", "Contact Us", "Rate in App Store", "Acknowledgements"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        backButton.tintColor = .black
+        
+        settingsTableView.delegate = self
+        settingsTableView.dataSource = self
+    }
+
+    @IBAction func backButtonPressed(_ sender: UIBarButtonItem) {
+        dismiss(animated: true, completion: nil)
+    }
+}
+
+extension SettingsViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("You tapped me!")
+    }
+}
+
+extension SettingsViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return settingsList.count
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "settingsTableViewCell", for: indexPath)
+        
+        cell.textLabel?.text = settingsList[indexPath.row]
+        return cell
     }
-    */
-
 }
