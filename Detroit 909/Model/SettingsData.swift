57508091a914fca85e12558b543242476d444681
Matt Dolan <mdolan.code@gmail.com>
2021-02-15 18:43:36 -0800

    Create struct for SettingsData.

---
+++ Detroit 909/Model/SettingsData.swift
@@ -0,0 +1,13 @@
+//
+//  SettingsData.swift
+//  Detroit 909
+//
+//  Created by Matt Dolan External macOS on 2021-02-15.
+//  Copyright © 2021 Matt Dolan External macOS. All rights reserved.
+//
+
+import Foundation
+
+struct SettingsData {
+    let  dataArray = ["FAQ", "Contact Us", "Rate in App Store", "Acknowledgements"]
+}
