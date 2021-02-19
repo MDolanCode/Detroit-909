9731c93f6f1ae0a498f58c0ff8208489283d1f87
Matt Dolan <mdolan.code@gmail.com>
2021-02-19 13:12:43 -0800

    Create FAQTableViewCell and outlets for xib questionLabel and answerLabel.

---
+++ Detroit 909/View/FAQ/FAQTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  FAQTableViewCell.swift
+//  Detroit 909
+//
+//  Created by Matt Dolan External macOS on 2021-02-19.
+//  Copyright © 2021 Matt Dolan External macOS. All rights reserved.
+//
+
+import UIKit
+
+class FAQTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var answerLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
