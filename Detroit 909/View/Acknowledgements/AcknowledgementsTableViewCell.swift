3bca0526dd4f243b61053e33304c821d4d9e85fd
Matt Dolan <mdolan.code@gmail.com>
2021-02-28 10:02:53 -0800

    Create acknowledgement swift file.

---
+++ Detroit 909/View/Acknowledgements/AcknowledgementsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  AcknowledgementsTableViewCell.swift
+//  Detroit 909
+//
+//  Created by Matt Dolan External macOS on 2021-02-28.
+//  Copyright © 2021 Matt Dolan External macOS. All rights reserved.
+//
+
+import UIKit
+
+class AcknowledgementsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
