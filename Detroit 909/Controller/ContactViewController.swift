c3990d1b28ca57979a358b39cd6cb9f75eaa24bf
Matt Dolan <mdolan.code@gmail.com>
2021-03-04 16:25:04 -0800

    Add ContactVC swift file.

---
+++ Detroit 909/Controller/ContactViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ContactViewController.swift
+//  Detroit 909
+//
+//  Created by Matt Dolan External macOS on 2021-03-04.
+//  Copyright © 2021 Matt Dolan External macOS. All rights reserved.
+//
+
+import UIKit
+
+class ContactViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
