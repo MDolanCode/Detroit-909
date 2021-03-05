14c639c5e5d20a25c858210765e76abf451640b8
Matt Dolan <mdolan.code@gmail.com>
2021-03-04 16:53:07 -0800

    Add MailViewController and Delegate to ContactVC. Add mail func and dismiss vc.

--- Detroit 909/Controller/ContactViewController.swift
+++ Detroit 909/Controller/ContactViewController.swift
@@ -7,12 +7,39 @@
 //
 
 import UIKit
+import MessageUI
 
-class ContactViewController: UIViewController {
+class ContactViewController: UIViewController, MFMailComposeViewControllerDelegate {
 
+    @IBOutlet weak var contactLabel: UILabel!
+    @IBOutlet weak var contactButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
+    
+    
+    @IBAction func contactButtonPressed(_ sender: UIButton) {
+        sendEmail()
+    }
+    
+    func sendEmail() {
+        if MFMailComposeViewController.canSendMail() {
+            let mail = MFMailComposeViewController()
+            mail.mailComposeDelegate = self
+            mail.setToRecipients(["matt@mdolancode.com"])
+            mail.setMessageBody("<p>You're soooo awesome!</p>", isHTML: true)
+            
+            present(mail, animated: true)
+        } else {
+            print("Email cannot send")
+        }
+    }
+    
+    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+        controller.dismiss(animated: true, completion: nil)
+    }
+    
+  
 }
