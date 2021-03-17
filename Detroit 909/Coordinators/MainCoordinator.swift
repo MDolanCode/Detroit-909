745b39dda3069241ccf26f06f7177d7f897d03be
Matt Dolan <mdolan.code@gmail.com>
2021-03-17 14:29:22 -0700

    Add MainCoordinator class.

---
+++ Detroit 909/Coordinators/MainCoordinator.swift
@@ -0,0 +1,25 @@
+//
+//  MainCoordinator.swift
+//  Detroit 909
+//
+//  Created by Matt Dolan External macOS on 2021-03-16.
+//  Copyright © 2021 Matt Dolan External macOS. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MainCoordinator: Coordinator {
+    var childCoordinators = [Coordinator]()
+    var navigationController: UINavigationController
+    
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+    
+    func start() {
+        let vc = Detroit909ViewController.instantiate()
+        vc.coordinator = self
+        navigationController.pushViewController(vc, animated: false)
+    }
+}
