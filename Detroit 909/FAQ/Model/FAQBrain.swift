d71dda30c5599c1f309059b8386bae5a87af98e3
Matt Dolan <mdolan.code@gmail.com>
2021-03-24 11:31:36 -0700

    Add question and answer to FAQBrain.

--- Detroit 909/FAQ/Model/FAQBrain.swift
+++ Detroit 909/FAQ/Model/FAQBrain.swift
@@ -12,6 +12,7 @@ struct FAQBrain {
     let faq = [
         FAQ(question: "Why Detroit 909?" , answer: "I created Detroit 909 to pay homage to the classic 80's drum machine from Roland the TR909 and Detroit, the birthplace of Techno music. As a child, I grew up in Windsor, Canada, a neighbouring city of Detroit, and was where I would get most of my music I would hear from the radio stations. Here is where I would first hear the Detroit sounds."),
         FAQ(question: "Can I use Detroit 909 with other iOS apps?", answer: "This feature will be available in the future."),
+        FAQ(question: "What features are coming up?", answer: "One feature will be a drum sequencer to create patterns."),
         FAQ(question: "Can I give you suggestions for a cool new feature?", answer: "Of course, you can! If you go into Settings and tap on Contact Us, you can send me an email with any cool ideas you have!"),
         FAQ(question: "Who is the team behind Detroit 909?", answer: "Hi there! That would be me! My name is Matt, and I am an iOS Engineer from Canada, now living and working on the west coast in Vancouver. I am passionate about app development and making techno music, which led me to build this app. Other than coding and techno music, I love travelling to Japan and studying the language and culture. Plus, playing the bagpipe.")
     ]
