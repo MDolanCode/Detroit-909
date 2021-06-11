55edc1689bfdf4e3481a430a2a580557b182df77
Matt Dolan <mdolan.code@gmail.com>
2021-06-11 06:33:03 -0700

    Delete dead code and commented out code.

--- Detroit 909/ShoutOuts/View/ShoutOutsTableViewCell.swift
+++ Detroit 909/ShoutOuts/View/ShoutOutsTableViewCell.swift
@@ -22,15 +22,9 @@ class ShoutOutsTableViewCell: UITableViewCell {
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-        // Configure the view for the selected state
-    }
-    
-    @IBAction func linkButtonPressed(_ sender: UIButton) {
-        UIApplication.shared.open(URL(string: "https://mdolancode.com")!, options: [:], completionHandler: nil)
     }
 }
