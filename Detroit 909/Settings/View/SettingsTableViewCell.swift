f5c843dc44e9e721962a6e3356f87c162e76d0f4
Matt Dolan <mdolan.code@gmail.com>
2021-06-11 06:37:16 -0700

    Delete dead and commented out code.

--- Detroit 909/Settings/View/SettingsTableViewCell.swift
+++ Detroit 909/Settings/View/SettingsTableViewCell.swift
@@ -21,18 +21,9 @@ class SettingsTableViewCell: UITableViewCell {
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
     }
-    
-    @IBAction func cellButtonPressed(_ sender: UIButton) {
-        
-    }
-
-    
 }
